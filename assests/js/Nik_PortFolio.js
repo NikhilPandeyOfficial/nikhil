f9baa30ab2b3335236faed37b4f7597826c6b690
NikhilPandeyOfficial <nikhilpandey1018@gmail.com>
2019-07-03 21:45:47 +0530

    'message'

--- assests/js/Nik_PortFolio.js
+++ assests/js/Nik_PortFolio.js
@@ -1,13 +1,6 @@
 var arr = ['primary', 'secondary', 'info', 'dark', 'danger', 'warning', 'success']
 var arr1 = ['lobster', 'Indie Flower', 'Pacifico'];
 
-
-
-
-
-
-
-
 $(".fa-plus").click(function(){
 	$(".forming").toggle();
 
@@ -19,8 +12,8 @@ $(".forming").on("click", ".btn-primary", function(event){
 	if(visiterName.length >= 1 && visiterComment.length >= 1){
 		var randomColor = arr[Math.floor(Math.random() * 7)];
 		var textStyle = arr1[Math.floor(Math.random() * 3)];
-		$(".Name").val("");
-		$(".Comment").val("");
+		// $(".Name").val("");
+		// $(".Comment").val("");
 		$("ul .cardList").append("<li><div class='card text-white bg-"+ 
 			randomColor +" mb-3' style='font-family: " + textStyle +" !important'><div class='card-header'>" + visiterName
 			 + "</div><div class='card-body'><p class='card-text'>" + 
